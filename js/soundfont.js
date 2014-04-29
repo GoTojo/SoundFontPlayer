1aa2f4e18c9201bb2926369169b936581f2c1349
gotojo <MXI03066@gmail.com>
2014-04-30 00:18:05 +0900

    pdataチャンクの取り込みができた ToDo:pdataチャンクとsdataチャンクから1音色分のパラメーターを抽出する

--- js/soundfont.js
+++ js/soundfont.js
@@ -21,7 +21,6 @@ Soundfont.prototype = {
 	getLists:function(buf) {
 		var ptr=0;
 		this.lists=[];
-		this.property.init();
 		// get and check
 		if (String.fromCharCode(buf[ptr++],buf[ptr++],buf[ptr++],buf[ptr++]) != "RIFF") { 
 			alert("can't find 'RIFF'"); 
@@ -44,18 +43,29 @@ Soundfont.prototype = {
 		this.getProperty(buf);
 		console.log(this.property);
 		// get preset
+		console.log(this.preset);
+		//for (key in this.lists["pdta"].chunks) {
+		//	var achunkinfo=this.lists["pdta"].chunks[key];
+		//	var ptr=achunkinfo.datatop;
+		//	var data="";
+		//	for (i=0;i<achunkinfo.size;i++) {
+		//		data+=buf[ptr++].toString(16);
+		//	}
+		//	console.log("key:"+key+",data:"+data);
+		//}
+		this.getPreset(buf);
 	},
 	getProperty:function(buf) {
-		var ptr;
+		this.property.init();
 		var achunkinfo;
 		achunkinfo=this.lists["INFO"].chunks["ifil"];
 		if (achunkinfo) {
-			ptr=achunkinfo.datatop;
+			var ptr=achunkinfo.datatop;
 			this.property.version=buf[ptr++]+(buf[ptr++]<<8)+(buf[ptr++]<<16)+(buf[ptr++]<<24);
 		}
 		achunkinfo=this.lists["INFO"].chunks["INAM"];
 		if (achunkinfo) {
-			ptr=achunkinfo.datatop;
+			var ptr=achunkinfo.datatop;
 			var str="";
 			for(i=0;i<achunkinfo.size;i++) { 
 				str+=String.fromCharCode(buf[ptr++]); 
@@ -64,7 +74,7 @@ Soundfont.prototype = {
 		}
 		achunkinfo=this.lists["INFO"].chunks["isng"];
 		if (achunkinfo) {
-			ptr=achunkinfo.datatop;
+			var ptr=achunkinfo.datatop;
 			var str="";
 			for(i=0;i<achunkinfo.size;i++) { 
 				str+=String.fromCharCode(buf[ptr++]); 
@@ -73,7 +83,7 @@ Soundfont.prototype = {
 		}
 		achunkinfo=this.lists["INFO"].chunks["IENG"];
 		if (achunkinfo) {
-			ptr=achunkinfo.datatop;
+			var ptr=achunkinfo.datatop;
 			var str="";
 			for(i=0;i<achunkinfo.size;i++) { 
 				str+=String.fromCharCode(buf[ptr++]); 
@@ -82,7 +92,7 @@ Soundfont.prototype = {
 		}
 		achunkinfo=this.lists["INFO"].chunks["ICMT"];
 		if (achunkinfo) {
-			ptr=achunkinfo.datatop;
+			var ptr=achunkinfo.datatop;
 			var str="";
 			for(i=0;i<achunkinfo.size;i++) { 
 				str+=String.fromCharCode(buf[ptr++]); 
@@ -91,7 +101,7 @@ Soundfont.prototype = {
 		}
 		achunkinfo=this.lists["INFO"].chunks["ICOP"];
 		if (achunkinfo) {
-			ptr=achunkinfo.datatop;
+			var ptr=achunkinfo.datatop;
 			var str="";
 			for(i=0;i<achunkinfo.size;i++) { 
 				str+=String.fromCharCode(buf[ptr++]); 
@@ -99,11 +109,157 @@ Soundfont.prototype = {
 		}
 		this.property.copyright=str;
 	},
+	getPreset: function(buf) {
+		this.preset.init();
+		var pdta=this.lists["pdta"];
+		var achunkinfo;
+		var str="";
+		// header The Preset Headers
+		achunkinfo=pdta.chunks["phdr"];
+		if (achunkinfo) {
+			var ptr=achunkinfo.datatop;
+			var phdr=this.preset.phdr;
+			for(i=0;i<20;i++) {
+				phdr.name+=String.fromCharCode(buf[ptr++]);
+			}
+			phdr.preset=buf[ptr++]+(buf[ptr++]<<8);
+			phdr.bank=buf[ptr++]+(buf[ptr++]<<8);
+			phdr.index=buf[ptr++]+(buf[ptr++]<<8);
+			phdr.library=buf[ptr++]+(buf[ptr++]<<8)+(buf[ptr++]<<16)+(buf[ptr++]<<24);
+			phdr.genre=buf[ptr++]+(buf[ptr++]<<8)+(buf[ptr++]<<16)+(buf[ptr++]<<24);
+			phdr.morphology=buf[ptr++]+(buf[ptr++]<<8)+(buf[ptr++]<<16)+(buf[ptr++]<<24);
+		}
+		// pbag The Preset Index list
+		achunkinfo=pdta.chunks["pbag"];
+		if (achunkinfo) {
+			var ptr=achunkinfo.datatop;
+			while (ptr<achunkinfo.datatop+achunkinfo.size) {
+				var genIndex=buf[ptr++]+(buf[ptr++]<<8);
+				var modIndex=buf[ptr++]+(buf[ptr++]<<8);
+				this.preset.pbag.push({genIndex:genIndex,modIndex:modIndex});				
+			}
+		}
+		// pmod The Preset Modulator list
+		achunkinfo=pdta.chunks["pmod"];
+		if (achunkinfo) {
+			var ptr=achunkinfo.datatop;
+			while (ptr<achunkinfo.datatop+achunkinfo.size) {
+				var modSrc=buf[ptr++]+(buf[ptr++]<<8);
+				var modDest=buf[ptr++]+(buf[ptr++]<<8);
+				var modAmount=buf[ptr++]+(buf[ptr++]<<8);
+				var modAmountSrc=buf[ptr++]+(buf[ptr++]<<8);
+				var ModTarnsform=buf[ptr++]+(buf[ptr++]<<8);
+				this.preset.pmod.push(
+					{modSrc:modSrc,
+					modDest:modDest,
+					modAmount:modAmount,
+					modAmountSrc:modAmountSrc,
+					ModTarnsform:ModTarnsform
+					});							
+			}
+		}
+		// pgen The Preset Generator list
+		achunkinfo=pdta.chunks["pgen"];
+		if (achunkinfo) {
+			var ptr=achunkinfo.datatop;
+			while (ptr<achunkinfo.datatop+achunkinfo.size) {
+				var lo=buf[ptr++];
+				var hi=buf[ptr++];
+				var amount=buf[ptr++]+(buf[ptr++]<<8);
+				this.preset.pgen.push({rangeLo:lo,rangeHi:hi,amount:amount});				
+			}
+		}		
+		// inst The Instrument Names and Indices
+		achunkinfo=pdta.chunks["inst"];
+		if (achunkinfo) {
+			var ptr=achunkinfo.datatop;
+			while (ptr<achunkinfo.datatop+achunkinfo.size) {
+				var str="";
+				for(i=0;i<20;i++) {
+					str+=String.fromCharCode(buf[ptr++]); 
+				}
+				var index=buf[ptr++]+(buf[ptr++]<<8);
+				this.preset.inst.push({name:str,index:index});
+			}
+		}
+		// ibag The Instrument Index list
+		achunkinfo=pdta.chunks["ibag"];
+		if (achunkinfo) {
+			var ptr=achunkinfo.datatop;
+			while (ptr<achunkinfo.datatop+achunkinfo.size) {
+				var genIndex=buf[ptr++]+(buf[ptr++]<<8);
+				var modIndex=buf[ptr++]+(buf[ptr++]<<8);
+				this.preset.ibag.push({genIndex:genIndex,modIndex:modIndex});				
+			}
+		}		
+		// igen The Instrument Generator list
+		achunkinfo=pdta.chunks["igen"];
+		if (achunkinfo) {
+			var ptr=achunkinfo.datatop;
+			while (ptr<achunkinfo.datatop+achunkinfo.size) {
+				var lo=buf[ptr++];
+				var hi=buf[ptr++];
+				var amount=buf[ptr++]+(buf[ptr++]<<8);
+				this.preset.igen.push({rangeLo:lo,rangeHi:hi,amount:amount});				
+			}
+		}		
+		// imod The Instrument Modulator list
+		achunkinfo=pdta.chunks["imod"];
+		if (achunkinfo) {
+			var ptr=achunkinfo.datatop;
+			while (ptr<achunkinfo.datatop+achunkinfo.size) {
+				var modSrc=buf[ptr++]+(buf[ptr++]<<8);
+				var modDest=buf[ptr++]+(buf[ptr++]<<8);
+				var modAmount=buf[ptr++]+(buf[ptr++]<<8);
+				var modAmountSrc=buf[ptr++]+(buf[ptr++]<<8);
+				var ModTarnsform=buf[ptr++]+(buf[ptr++]<<8);
+				this.preset.imod.push(
+					{modSrc:modSrc,
+					modDest:modDest,
+					modAmount:modAmount,
+					modAmountSrc:modAmountSrc,
+					ModTarnsform:ModTarnsform
+					});							
+			}
+		}
+		// shdr The Sample Headers
+		achunkinfo=pdta.chunks["shdr"];
+		if (achunkinfo) {
+			var ptr=achunkinfo.datatop;
+			while (ptr<achunkinfo.datatop+achunkinfo.size) {
+				var name="";
+				for(i=0;i<20;i++) {
+					name+=String.fromCharCode(buf[ptr++]); 
+				}
+				var st=buf[ptr++]+(buf[ptr++]<<8)+(buf[ptr++]<<16)+(buf[ptr++]<<24);
+				var ed=buf[ptr++]+(buf[ptr++]<<8)+(buf[ptr++]<<16)+(buf[ptr++]<<24);
+				var stlp=buf[ptr++]+(buf[ptr++]<<8)+(buf[ptr++]<<16)+(buf[ptr++]<<24);
+				var edlp=buf[ptr++]+(buf[ptr++]<<8)+(buf[ptr++]<<16)+(buf[ptr++]<<24);
+				var rate=buf[ptr++]+(buf[ptr++]<<8)+(buf[ptr++]<<16)+(buf[ptr++]<<24);
+				var orgpitch=buf[ptr++];
+				var pitchCorrection=buf[ptr++];
+				var link=buf[ptr++]+(buf[ptr++]<<8);
+				var type=buf[ptr++]+(buf[ptr++]<<8);
+				this.preset.shdr.push(
+					{
+						start:st,
+						end:ed,
+						startLoop:stlp,
+						endLoop:edlp,
+						sampleRate:rate,
+						originalPitch:orgpitch,
+						pitchCorrection:pitchCorrection,
+						sampleLink:link,
+						sampleType:type
+					});
+			}
+		}
+	},
 	// private
 	lists:[],
 	property: {
-		version:256,
-		name:"hello world",
+		version:0,
+		name:"",
 		engine:"",
 		engineer:"",
 		tools:"",
@@ -112,12 +268,34 @@ Soundfont.prototype = {
 		init:function() {
 			this.version=0;
 			this.name="";
-			this.engine="",
-			this.engineer="",
-			this.tools="",
-			this.comment="",
-			this.copyright=""
-		},
+			this.engine="";
+			this.engineer="";
+			this.tools="";
+			this.comment="";
+			this.copyright="";
+		}
+	},
+	preset: {
+		inst:[], // {name:str,index:index}
+		pbag:[], // {rangeLo:lo,rangeHi:hi,amount:amount}
+		pgen:[], // {genIndex:genIndex,modIndex:modIndex}
+		phdr:{name:"",preset:0,bank:0,index:0,library:0,genre:0,morphology:0},
+		pmod:[], // {modDest:,modDest:,modAmount:,modAmountSrc:,ModTarnsform:}
+		ibag:[], // {genIndex:0,modIndex:0}
+		imod:[], // {modDest:,modDest:,modAmount:,modAmountSrc:,ModTarnsform:}
+		igen:[], // {genIndex:genIndex,modIndex:modIndex}
+		shdr:[],
+		init:function() {
+			ibag=[];
+			igen=[];
+			imod=[];
+			inst=[];
+			pbag=[{genNdx:0,modNdx:0}];
+			pgen=[];
+			phdr={name:"",bank:0,preset:0,index:0,library:0,genre:0,morphology:0};
+			pmod=[];
+			shdr=[];
+		}
 	},
 	getListChunk:function(ptr) {
 		var buf=fileinfo.buf;
